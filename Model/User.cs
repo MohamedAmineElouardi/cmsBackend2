2ffb82f0170e13878b81b5a1b32b661ca65612c5
Mohamed-Amine EL OUARDI <maelouardi@dicastalma.com>
2024-08-09 15:17:03 +0100

    first commit

---
+++ Model/User.cs
@@ -0,0 +1,21 @@
+﻿using System.Data;
+
+namespace CmsBackend.Model
+{
+    public class User
+    {
+       
+
+
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public Role Role { get; set; }
+            public int? RoleId { get; set; } // Foreign key to Role
+            public string Cin { get; set; }
+            public DateTime? DateOfBirth { get; set; }
+            public string Matricule { get; set; }
+            public string Password { get; set; }
+            public string Username { get; set; }
+        }
+   
+}
